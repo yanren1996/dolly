6c39b3693dd8e29843e5c4cfc37a635821ac6a7d
yanren <easy10441@gmail.com>
2025-01-03 11:34:01 +0800

    先配置H2資料庫+security身分驗證鏈，並允許H2免授權免csrf

---
+++ src/main/java/com/example/dolly/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.example.dolly.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // 所有請求需驗證，另外列白名單
+                .authorizeHttpRequests(auth -> {
+                    auth
+                            .requestMatchers("/public/*").permitAll()
+                            .requestMatchers(PathRequest.toH2Console()).permitAll()
+                            .anyRequest().authenticated()
+                    ;
+                })
+                .formLogin(Customizer.withDefaults())
+                // 以下為h2-console配置 1.允許iframe 2.無須csrf_token
+                .headers(headers -> headers.frameOptions(FrameOptionsConfig::sameOrigin))
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers(PathRequest.toH2Console())
+                )
+        ;
+        return http.build();
+    }
+}
---
+++ src/main/java/com/example/dolly/controller/PublicController.java
@@ -0,0 +1,15 @@
+package com.example.dolly.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public")
+public class PublicController {
+
+    @GetMapping("/h")
+    String hello() {
+        return "hello world!";
+    }
+}
