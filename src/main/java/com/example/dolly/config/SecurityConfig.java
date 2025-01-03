d010522a38a5aa1dc28ef4047ec9d3639872773b
yanren <easy10441@gmail.com>
2025-01-03 11:57:35 +0800

    新增:用戶登入功能 (預設資料表，未來再客製)

--- src/main/java/com/example/dolly/config/SecurityConfig.java
+++ src/main/java/com/example/dolly/config/SecurityConfig.java
@@ -7,7 +7,15 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -31,5 +39,25 @@ public class SecurityConfig {
                 )
         ;
         return http.build();
+    }
+
+    // TODO:暫時簡化用spring提供的資料結構
+    @Bean
+    UserDetailsManager userDetailsService(DataSource dataSource) {
+        UserDetails user1 = User.builder()
+                .username("yanren")
+                .password("{noop}yanren")
+                .roles("USER")
+                .build();
+        UserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        if (!userDetailsManager.userExists(user1.getUsername())) {
+            userDetailsManager.createUser(user1);
+        }
+        return userDetailsManager;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
