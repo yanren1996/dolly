d9765f0bc61e3d907ac8f39ea46e22f9f350b260
yanren <easy10441@gmail.com>
2025-01-03 10:57:23 +0800

    spring init

---
+++ src/test/java/com/example/dolly/DollyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dolly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DollyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
